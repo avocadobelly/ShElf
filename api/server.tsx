43d369e77b51f4af4b7c01ed2a485e20582524cb
Georgia <email.g@hey.com>
2022-06-29 17:57:32 +0100

    Adding functionality for cors to allow frontend and backend on different ports to communicate

--- api/server.tsx
+++ api/server.tsx
@@ -1,3 +1,14 @@
 /*This is the backend Node server for the ShElf Shopping list application*/
 const express = require('express');
+var cors = require('cors');
 
+const API_PORT = 4000;
+const app = express();
+
+// CORS enables access to a resource from a different origin (i.e: the client)
+// When the client frontend requests a resource, the response will additionally contain a stamp that tells your browser to allow resource sharing across different origins.
+app.use(cors());
+
+app.listen(API_PORT, () => {
+    console.log('listening on port' + API_PORT);
+})
