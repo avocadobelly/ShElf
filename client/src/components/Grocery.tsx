d6a9b43397ef3705eae5cc549a173aaccefd9dbc
Georgia <email.g@hey.com>
2022-06-30 21:18:26 +0100

    Adding some styles to list item

--- client/src/components/Grocery.tsx
+++ client/src/components/Grocery.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Grocery: React.FC<IGroceryProps> = ({grocery}) => {
-    return (<li>{grocery.name}</li>);
+    return (<li className='text-left list-none m-3 py-3'>{grocery.name}</li>);
   };
