9d7bc42841b5ee425a4690589278abfe48495a96
Georgia <email.g@hey.com>
2022-06-30 18:37:56 +0100

    Added example list of groceries and a new grocery component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
-import './App.css';
+import { any } from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import API from "./API";
+import { Grocery } from './components/Grocery';
+import GroceryList from "./components/GroceryList"
 
 function App() {
+
+    const exampleGroceries: IGrocery[] = [
+        {
+           _id: '123',
+           name: 'Banana',
+           bought: false, 
+        },
+        {
+            _id: '456',
+            name: 'Apple',
+            bought: true, 
+        },
+        {
+            _id: '789',
+            name: 'Bread',
+            bought: false, 
+        },
+      ];
+
+    const [groceries, setGroceries] = useState(exampleGroceries);
+    // const [grocery, setGrocery] = useState("");
+
+
+    // useEffect(() => {
+    //     const fetchGroceryAndSetGroceries = async () => {
+    //         const groceries = await API.getAllGroceries();
+    //         setGroceries(groceries);
+    //     }
+    //     fetchGroceryAndSetGroceries();
+    // }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className='text-8xl font-bold text-amber-600'>ShElf</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+        <main>
+            <h1 className='text-8xl font-bold text-green-500'>ShElf</h1>
+            <h2 className='text-6xl font-semibold text-green-800'>Grocery List</h2>
+            <ul>
+                <Grocery grocery={exampleGroceries[0]}/>
+                <Grocery grocery={exampleGroceries[1]}/>
+                <Grocery grocery={exampleGroceries[2]}/>
+            </ul>
+        </main>
+    </div>        
   );
 }
 
--- client/src/components/Grocery.tsx
+++ client/src/components/Grocery.tsx
@@ -1,14 +1,5 @@
 import React from "react";
 
-interface IGrocery {
-    name: string;
-    bought: boolean;
-}
-
-interface Props {
-    grocery: IGrocery;
-}
-
-export const Grocery: React.FC<Props> = ({grocery}) => {
+export const Grocery: React.FC<IGroceryProps> = ({grocery}) => {
     return (<li>{grocery.name}</li>);
   };
