b2c98d0638d5c7eb2591bb10076d779394f2410d
Georgia <email.g@hey.com>
2022-06-30 20:42:58 +0100

    Added some tailwind classes to style the headings and list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -37,15 +37,19 @@ function App() {
     // }, [])
 
   return (
-    <div className="App">
-        <main>
-            <h1 className='text-8xl font-bold text-green-500'>ShElf</h1>
-            <h2 className='text-6xl font-semibold text-green-800'>Grocery List</h2>
-            <ul>
-                <Grocery grocery={exampleGroceries[0]}/>
-                <Grocery grocery={exampleGroceries[1]}/>
-                <Grocery grocery={exampleGroceries[2]}/>
-            </ul>
+    <div className="App grid grid-cols-5 grid-rows-6'">
+        <main className='col-span-full row-span-full'>
+            <div className='col-start-2 col-span-3 gap-4 grid-rows-1 row-span-2'>
+                <h1 className='text-center text-8xl font-bold text-green-500'>ShElf</h1>
+                <h2 className='text-center text-6xl font-semibold text-green-800'>Grocery List</h2>
+            </div>
+            <div className='groceries flex justify-center col-start-2 col-span-3 gap-4 grid-rows-3 row-span-4'>
+                <ul className=''>
+                    <Grocery grocery={exampleGroceries[0]}/>
+                    <Grocery grocery={exampleGroceries[1]}/>
+                    <Grocery grocery={exampleGroceries[2]}/>
+                </ul>
+            </div>
         </main>
     </div>        
   );
